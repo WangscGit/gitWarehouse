21a159b6cfe8fcf8650b0c23c3aebc04433f39ae
wangsc <wangsc@bjdihao.com.cn>
2018-07-05 15:28:28 +0800

    yrdy

---
+++ cms_cloudyTest/src/main/java/com/cms_cloudy/dao/impl/ProComponentDaoImpl.java
@@ -0,0 +1,8 @@
+package com.cms_cloudy.dao.impl;
+
+import org.springframework.stereotype.Component;
+
+import com.cms_cloudy.dao.ProComponentDao;
+@Component(value="ProComponentDao")
+public class ProComponentDaoImpl implements ProComponentDao {
+}
